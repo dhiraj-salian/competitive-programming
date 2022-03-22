c7d28c5dfc9219e64cccbbf7d175f5c258bae21e
dhiraj-salian <dhirajsalian1996@gmail.com>
2022-03-22 08:28:32 +0530

    leetcode: 1. runningSum 2. minimumTimeToCollectAllApplesInTree

---
+++ src/main/java/com/dhirajsalian/cp/leetcode/runningSum/Solution.java
@@ -0,0 +1,12 @@
+// Problem: https://leetcode.com/problems/running-sum-of-1d-array/
+
+package com.dhirajsalian.cp.leetcode.runningSum;
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
