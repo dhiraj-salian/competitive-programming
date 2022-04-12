56a8911869cc8a665058e784a390ada3c970a627
dhiraj-salian <dhirajsalian1996@gmail.com>
2022-04-12 06:47:00 +0530

    leetcode: 1. findAllDuplicatesInAnArray 2. findTheDuplicateNumber

---
+++ src/main/java/com/dhirajsalian/cp/leetcode/findTheDuplicateNumber/Solution.java
@@ -0,0 +1,20 @@
+// Problem: https://leetcode.com/problems/find-the-duplicate-number/
+
+package com.dhirajsalian.cp.leetcode.findTheDuplicateNumber;
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
