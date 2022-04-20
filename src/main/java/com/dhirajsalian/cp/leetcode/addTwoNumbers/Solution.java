0a9102bf68ee15689d20045900414ecbd352c9fd
dhiraj-salian <dhirajsalian1996@gmail.com>
2022-04-20 07:25:28 +0530

    leetcode: 1. addTwoNumbers 2. removeNthNodeFromEndOfList

--- src/main/java/com/dhirajsalian/cp/leetcode/addTwoNumbers/Solution.java
+++ src/main/java/com/dhirajsalian/cp/leetcode/addTwoNumbers/Solution.java
@@ -6,8 +6,8 @@ public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode prev = null, head = null;
         int carryForward = 0;
-        while (l1 != null && l2 != null) {
-            int sum = l1.val + l2.val + carryForward;
+        while (l1 != null || l2 != null) {
+            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carryForward;
             if (prev == null) {
                 prev = new ListNode(sum % 10);
                 head = prev;
@@ -16,24 +16,10 @@ public class Solution {
                 prev = prev.next;
             }
             carryForward = sum / 10;
-            l1 = l1.next;
-            l2 = l2.next;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
-        while (l1 != null) {
-            int sum = l1.val + carryForward;
-            prev.next = new ListNode(sum % 10);
-            carryForward = sum / 10;
-            l1 = l1.next;
-            prev = prev.next;
-        }
-        while (l2 != null) {
-            int sum = l2.val + carryForward;
-            prev.next = new ListNode(sum % 10);
-            carryForward = sum / 10;
-            l2 = l2.next;
-            prev = prev.next;
-        }
-        if (l1 == null && l2 == null && carryForward != 0)
+        if (carryForward > 0)
             prev.next = new ListNode(carryForward);
         return head;
     }
