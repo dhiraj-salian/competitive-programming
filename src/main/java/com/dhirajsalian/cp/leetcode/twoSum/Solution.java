115952c013a11c89d206f08314f5691b84877911
dhiraj-salian <dhirajsalian1996@gmail.com>
2022-04-27 07:09:40 +0530

    leetcode: 1. mergeTwoSortedLists 2. squaresOfSortedArray 3. twoSum

---
+++ src/main/java/com/dhirajsalian/cp/leetcode/twoSum/Solution.java
@@ -0,0 +1,18 @@
+// Problem: https://leetcode.com/problems/two-sum/
+
+package com.dhirajsalian.cp.leetcode.twoSum;
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else
+                map.put(nums[i], i);
+        }
+        return new int[2];
+    }
+}
