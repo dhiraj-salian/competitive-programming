b060cc729b3ebafb16958bee8c4e336aee8da631
dhiraj-salian <dhirajsalian1996@gmail.com>
2022-04-29 07:30:59 +0530

    leetcode: 1. subarrayProductLessThanK 2. threesumClosest

---
+++ src/main/java/com/dhirajsalian/cp/leetcode/subarrayProductLessThanK/Solution.java
@@ -0,0 +1,21 @@
+// Problem: https://leetcode.com/problems/subarray-product-less-than-k/
+
+package com.dhirajsalian.cp.leetcode.subarrayProductLessThanK;
+
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int start = 0, end = 0;
+        long product = 1;
+        int count = 0;
+        while(end < nums.length) {
+            product *= nums[end];
+            while(start<=end && product >= k) {
+                product/=nums[start];
+                start++;
+            }
+            count += end - start + 1;
+            end++;
+        }
+        return count;
+    }
+}
