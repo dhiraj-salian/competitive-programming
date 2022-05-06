6e9a85062c28bad4eb63391d4aa6acbf590194aa
dhiraj-salian <dhirajsalian1996@gmail.com>
2022-05-06 07:32:41 +0530

    leetcode: 1. longestRepeatingCharacterReplacement 2. longestSubstringWithoutRepeatingCharacter

---
+++ src/main/java/com/dhirajsalian/cp/leetcode/longestSubstringWithoutRepeatingCharacter/Solution.java
@@ -0,0 +1,28 @@
+// Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+package com.dhirajsalian.cp.leetcode.longestSubstringWithoutRepeatingCharacter;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int start = 0, end = 0;
+        int[] charCount = new int[256];
+        int res = 0;
+        while (end < s.length()) {
+            charCount[s.charAt(end)]++;
+            while (start <= end && isRepeating(charCount)) {
+                charCount[s.charAt(start)]--;
+                start++;
+            }
+            res = Math.max(res, end - start + 1);
+            end++;
+        }
+        return res;
+    }
+
+    boolean isRepeating(int[] charCount) {
+        for (int i = 0; i < 256; i++)
+            if (charCount[i] > 1)
+                return true;
+        return false;
+    }
+}
