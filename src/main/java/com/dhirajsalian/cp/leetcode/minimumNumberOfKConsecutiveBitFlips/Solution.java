85f99b89b3c8cd912bbe3e815675e0c556f8ab89
dhiraj-salian <dhirajsalian1996@gmail.com>
2022-05-11 09:26:07 +0530

    leetcode: 1. minimumNumberOfKConsecutiveBitFlips

---
+++ src/main/java/com/dhirajsalian/cp/leetcode/minimumNumberOfKConsecutiveBitFlips/Solution.java
@@ -0,0 +1,23 @@
+// Problem: https://leetcode.com/problems/minimum-number-of-k-consecutive-bit-flips/
+
+package com.dhirajsalian.cp.leetcode.minimumNumberOfKConsecutiveBitFlips;
+
+class Solution {
+    public int minKBitFlips(int[] nums, int k) {
+        int start = 0, end = 0, flips = 0, res = 0, lastFlip = -1;
+        while (end < nums.length) {
+            if (nums[end] == flips % 2) {
+                flips++;
+                lastFlip = end;
+                res++;
+                nums[end] = -1;
+            }
+            if (end - start + 1 >= k) {
+                if (nums[start] < 0) flips--;
+                start++;
+            }
+            end++;
+        }
+        return nums.length - k >= lastFlip ? res : -1;
+    }
+}
