fb09dab370432c8077eaa1e655821b6df242dcd5
dhiraj-salian <dhirajsalian1996@gmail.com>
2022-06-08 08:26:48 +0530

    leetcode: 1. binaryTreeRightSideView 2. allNodesDistanceKInBinaryTree

---
+++ src/main/java/com/dhirajsalian/cp/leetcode/binaryTreeRightSideView/Solution.java
@@ -0,0 +1,47 @@
+// Problem: https://leetcode.com/problems/binary-tree-right-side-view/
+
+package com.dhirajsalian.cp.leetcode.binaryTreeRightSideView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) return Collections.emptyList();
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int qSize = q.size();
+            for (int i = 0; i < qSize; i++) {
+                TreeNode curr = q.poll();
+                if (curr.left != null) q.add(curr.left);
+                if (curr.right != null) q.add(curr.right);
+                if (i == qSize - 1) result.add(curr.val);
+            }
+        }
+        return result;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
