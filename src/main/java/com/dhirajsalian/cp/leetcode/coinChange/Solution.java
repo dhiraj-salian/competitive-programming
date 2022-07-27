70a521981a763dfb262ff679294002a728c467f9
dhiraj-salian <dhirajsalian1996@gmail.com>
2022-07-27 08:27:08 +0530

    leetcode: 1. coinChange 2. houseRobber2

---
+++ src/main/java/com/dhirajsalian/cp/leetcode/coinChange/Solution.java
@@ -0,0 +1,37 @@
+// Problem: https://leetcode.com/problems/coin-change/
+
+package com.dhirajsalian.cp.leetcode.coinChange;
+
+import java.util.Arrays;
+
+class Solution {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] change = new int[amount + 1];
+        Arrays.fill(change, Integer.MAX_VALUE);
+        change[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0 && change[i - coins[j]] != Integer.MAX_VALUE) {
+                    change[i] = Math.min(change[i], 1 + change[i - coins[j]]);
+                }
+            }
+        }
+        return change[amount] == Integer.MAX_VALUE ? -1 : change[amount];
+    }
+
+    /*
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        int minCount = Integer.MAX_VALUE, prevCount;
+        for (int i = 0; i < coins.length; i++) {
+            if (amount - coins[i] >= 0) {
+                prevCount = coinChange(coins, amount - coins[i]);
+                if (prevCount != -1)
+                    minCount = Math.min(minCount, 1 + prevCount);
+            }
+        }
+        return minCount == Integer.MAX_VALUE ? -1 : minCount;
+    }
+    */
+}
---
+++ src/main/java/com/dhirajsalian/cp/leetcode/houseRobber2/Solution.java
@@ -0,0 +1,29 @@
+// Problem: https://leetcode.com/problems/house-robber-ii/
+
+package com.dhirajsalian.cp.leetcode.houseRobber2;
+
+class Solution {
+
+    public int rob(int[] nums) {
+        if(nums.length==0) return 0;
+        if(nums.length==1) return nums[0];
+        return Math.max(rob(nums, 0, nums.length - 2), rob(nums, 1, nums.length - 1));
+    }
+
+    public int rob(int[] nums, int start, int end) {
+        int theft1 = 0, theft2 = nums[start], temp;
+        for (int i = start; i <= end; i++) {
+            temp = Math.max(nums[i] + theft1, theft2);
+            theft1 = theft2;
+            theft2 = temp;
+        }
+        return theft2;
+    }
+
+    /*
+    public int rob(int[] nums, int n) {
+        if (n == 0) return 0;
+        return Math.max(nums[n - 1] + rob(nums, n - 2), rob(nums, n - 1));
+    }
+     */
+}
