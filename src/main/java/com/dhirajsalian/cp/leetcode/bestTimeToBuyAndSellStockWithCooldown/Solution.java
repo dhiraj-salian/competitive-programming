7d2ced79bd2ebde1393cfc7a97d2195db28b731b
dhiraj-salian <dhirajsalian1996@gmail.com>
2022-08-03 09:13:53 +0530

    leetcode: 1. bestTimeToBuyAndSellStockWithCooldown 2. partitionEqualSubsetSum

---
+++ src/main/java/com/dhirajsalian/cp/leetcode/bestTimeToBuyAndSellStockWithCooldown/Solution.java
@@ -0,0 +1,19 @@
+// Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+
+package com.dhirajsalian.cp.leetcode.bestTimeToBuyAndSellStockWithCooldown;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[] profit = new int[prices.length + 1];
+        profit[0] = 0;
+        for (int i = 1; i <= prices.length; i++) {
+            profit[i] = profit[i - 1];
+            for (int j = 0; j < i - 1; j++) {
+                if (prices[i - 1] - prices[j] > 0)
+                    profit[i] = Math.max(profit[i], prices[i - 1] - prices[j]
+                            + (j - 1 >= 0 ? profit[j - 1] : 0));
+            }
+        }
+        return profit[prices.length];
+    }
+}
