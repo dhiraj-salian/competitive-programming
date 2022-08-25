10ea971baab61b4ac3ccac46f34ccbe683d158bb
dhiraj-salian <dhirajsalian1996@gmail.com>
2022-08-25 08:53:06 +0530

    leetcode: 1. numberOfIslands 2. pacificAtlantic

--- src/main/java/com/dhirajsalian/cp/leetcode/problemPatterns/numberOfIslands/Solution.java
+++ src/main/java/com/dhirajsalian/cp/leetcode/problemPatterns/numberOfIslands/Solution.java
@@ -2,6 +2,11 @@
 
 package com.dhirajsalian.cp.leetcode.problemPatterns.numberOfIslands;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 class Solution {
     public int numIslands(char[][] grid) {
         int res = 0;
@@ -11,11 +16,33 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
                     res++;
-                    dfs(i, j, m, n, grid);
+                    bfs(i, j, m, n, grid);
+                    //dfs(i, j, m, n, grid);
                 }
             }
         }
         return res;
+    }
+
+    private void bfs(int i, int j, int m, int n, char[][] grid) {
+        Queue<List<Integer>> q = new LinkedList<>();
+        q.add(Arrays.asList(i, j));
+        grid[i][j] = '2';
+        while (!q.isEmpty()) {
+            List<Integer> curr = q.poll();
+            int ci = curr.get(0), cj = curr.get(1);
+            visit(i - 1, j, m, n, grid, q);
+            visit(i + 1, j, m, n, grid, q);
+            visit(i, j - 1, m, n, grid, q);
+            visit(i, j + 1, m, n, grid, q);
+        }
+    }
+
+    private void visit(int i, int j, int m, int n, char[][] grid, Queue<List<Integer>> q) {
+        if (i >= 0 && i < m && j >= 0 && j < n && grid[i][j] != '2') {
+            grid[i][j] = '2';
+            q.add(Arrays.asList(i, j));
+        }
     }
 
     private void dfs(int i, int j, int m, int n, char[][] grid) {
--- src/main/java/com/dhirajsalian/cp/leetcode/problemPatterns/pacificAtlanticWaterFlow/Solution.java
+++ src/main/java/com/dhirajsalian/cp/leetcode/problemPatterns/pacificAtlanticWaterFlow/Solution.java
@@ -4,9 +4,59 @@ package com.dhirajsalian.cp.leetcode.problemPatterns.pacificAtlanticWaterFlow;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class Solution {
+
+    // BFS Approach
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        List<List<Integer>> result = new ArrayList<>();
+        int m = heights.length, n = heights[0].length;
+        boolean[][] flowsPacific = new boolean[m][n];
+        boolean[][] flowsAtlantic = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            flows(i, 0, heights, m, n, flowsPacific);
+            flows(i, n - 1, heights, m, n, flowsAtlantic);
+        }
+        for (int i = 0; i < n; i++) {
+            flows(0, i, heights, m, n, flowsPacific);
+            flows(m - 1, i, heights, m, n, flowsAtlantic);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (flowsAtlantic[i][j] && flowsPacific[i][j]) result.add(Arrays.asList(i, j));
+            }
+        }
+        return result;
+    }
+
+    public void flows(int i, int j, int[][] heights, int m, int n, boolean[][] visited) {
+        Queue<List<Integer>> q = new LinkedList<>();
+        q.add(Arrays.asList(i, j));
+        visited[i][j] = true;
+        while (!q.isEmpty()) {
+            List<Integer> curr = q.poll();
+            int currI = curr.get(0), currJ = curr.get(1);
+            int currHeight = heights[currI][currJ];
+            visit(heights, visited, q, currI - 1, currJ, currHeight, m, n);
+            visit(heights, visited, q, currI + 1, currJ, currHeight, m, n);
+            visit(heights, visited, q, currI, currJ - 1, currHeight, m, n);
+            visit(heights, visited, q, currI, currJ + 1, currHeight, m, n);
+        }
+    }
+
+    private void visit(int[][] heights, boolean[][] visited, Queue<List<Integer>> q, int i, int j, int prevHeight, int m, int n) {
+        if (i >= 0 && i < m && j >= 0 && j < n && prevHeight <= heights[i][j] && !visited[i][j]) {
+            visited[i][j] = true;
+            q.add(Arrays.asList(i, j));
+        }
+    }
+
+
+    /*
+    DFS Approach
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         List<List<Integer>> res = new ArrayList<>();
         int m = heights.length, n = heights[0].length;
@@ -36,5 +86,7 @@ class Solution {
         flows(i - 1, j, height[i][j], visited, m, n, height);
         flows(i + 1, j, height[i][j], visited, m, n, height);
     }
+
+     */
 }
 
